9f7f9bab699b21231643b0ec16e1a081e21d27dc
Litfa <admin@litf.com.cn>
2022-03-09 11:31:17 +0800

    feat(user): 获取用户信息

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,19 @@
 /*
  * @Author: litfa
  * @Date: 2022-03-01 10:24:38
- * @LastEditTime: 2022-03-01 11:12:45
+ * @LastEditTime: 2022-03-08 10:09:23
  * @LastEditors: litfa
  * @Description: 路由
  * @FilePath: /blog-service/src/router/index.ts
  * 
  */
 import express from 'express'
-const router = express.Router() 
+const router = express.Router()
 
 import userLogin from './user/login'
+import getUserInfo from './user/getUserInfo'
 
-router.use('/login' , userLogin)
+router.use('/login', userLogin)
+router.use('/getUserInfo', getUserInfo)
 
 export default router
