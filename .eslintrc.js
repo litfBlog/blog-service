63518c88f8105538bc21c971d5dd4e7265f1ae9a
Litfa <admin@litf.com.cn>
2022-03-11 10:32:00 +0800

    chore(eslint): 允许any类型

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
     'no-empty': ['error', { allowEmptyCatch: true }],
 
     '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-unused-vars': 'off'
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
   }
 }
