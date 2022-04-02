a11fbac4b12407858a2e20fe2e1e89bce1f9546e
Litfa <admin@litf.com.cn>
2022-04-02 19:56:29 +0800

    feat(getOne): 点赞数据查询

--- src/router/articles/getOne.ts
+++ src/router/articles/getOne.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: litfa
  * @Date: 2022-03-22 10:43:53
- * @LastEditTime: 2022-03-23 19:57:57
+ * @LastEditTime: 2022-04-02 19:55:41
  * @LastEditors: litfa
  * @Description: 获取单个文章
  * @FilePath: /blog-service/src/router/articles/getOne.ts
@@ -11,9 +11,36 @@ import query from './../../db/query'
 import { Router } from 'express'
 const router = Router()
 
+const sql = `
+SELECT
+    articles.id,
+    articles.type,
+    articles.uuid,
+    articles.title,
+    articles.content,
+    articles.cover,
+    articles.status,
+    articles.author,
+    articles.createDate,
+    articles.desc,
+    author.avatar,
+    author.username,
+    COUNT(DISTINCT likes.id, likes.like) AS likes_count,
+    is_liked.\`user_id\` AS liked
+FROM
+    \`articles\` AS articles
+LEFT JOIN \`likes\` likes ON articles.id = likes.articles_id AND likes.like=1
+LEFT JOIN \`users\` author ON articles.\`author\` = author.\`id\`
+LEFT JOIN \`likes\` is_liked ON articles.id = likes.articles_id AND is_liked.\`user_id\` = 1
+WHERE articles.id=? AND articles.status=0
+GROUP BY articles.id
+ORDER BY articles.createDate DESC
+LIMIT 0, 100
+`
+
 router.post('/:id', async (req, res) => {
   const { id } = req.params
-  const [err, results] = await query('SELECT a.*,a.*,b.username, b.`avatar` FROM `articles` a JOIN `users` b ON a.`author`=b.id where a.id=? and a.status=0', id)
+  const [err, results] = await query(sql, id)
   console.log(err)
 
   if (err) return res.send({ status: 5 })
