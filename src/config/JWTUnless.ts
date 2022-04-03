e9bd45e072fd2f1c10f169b624c4a03315e8c720
Litfa <admin@litf.com.cn>
2022-04-03 21:11:32 +0800

    fix: 优化文章点赞获取

--- src/config/JWTUnless.ts
+++ src/config/JWTUnless.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: litfa
  * @Date: 2022-03-15 10:31:02
- * @LastEditTime: 2022-03-29 20:40:43
+ * @LastEditTime: 2022-04-03 17:43:58
  * @LastEditors: litfa
  * @Description: tokek 无需认证
  * @FilePath: /blog-service/src/config/JWTUnless.ts
@@ -12,5 +12,5 @@ export default [
   new RegExp(`^${config.baseUrl}/login/`),
   new RegExp(`^${config.baseUrl}/articles/get/`),
   /^\/static\//,
-  new RegExp(`${config.baseUrl}/articles/getOne`)
+  new RegExp(`${config.baseUrl}/articles/getOne/[0-9]`)
 ]
--- src/router/articles/getOne.ts
+++ src/router/articles/getOne.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: litfa
  * @Date: 2022-03-22 10:43:53
- * @LastEditTime: 2022-04-02 19:55:41
+ * @LastEditTime: 2022-04-03 21:05:48
  * @LastEditors: litfa
  * @Description: 获取单个文章
  * @FilePath: /blog-service/src/router/articles/getOne.ts
@@ -26,12 +26,12 @@ SELECT
     author.avatar,
     author.username,
     COUNT(DISTINCT likes.id, likes.like) AS likes_count,
-    is_liked.\`user_id\` AS liked
+    IF(is_liked.\`like\`=1, 1, '0') AS liked
 FROM
     \`articles\` AS articles
 LEFT JOIN \`likes\` likes ON articles.id = likes.articles_id AND likes.like=1
 LEFT JOIN \`users\` author ON articles.\`author\` = author.\`id\`
-LEFT JOIN \`likes\` is_liked ON articles.id = likes.articles_id AND is_liked.\`user_id\` = 1
+LEFT JOIN likes is_liked ON is_liked.\`articles_id\` = articles.\`id\` AND is_liked.\`user_id\` = ?
 WHERE articles.id=? AND articles.status=0
 GROUP BY articles.id
 ORDER BY articles.createDate DESC
@@ -40,11 +40,21 @@ LIMIT 0, 100
 
 router.post('/:id', async (req, res) => {
   const { id } = req.params
-  const [err, results] = await query(sql, id)
+  const [err, results] = await query(sql, [0, id])
   console.log(err)
 
   if (err) return res.send({ status: 5 })
   res.send({ status: 1, data: results[0] })
+})
+
+router.post('/detailed/:id', async (req, res) => {
+  const { id } = req.params
+  const user = req.user as any
+  if (!user.id) return res.send({ status: 4 })
+  const [err, results] = await query(sql, [user.id, id])
+  if (err) return res.send({ status: 5 })
+  res.send({ status: 1, data: results[0] })
+
 })
 
 export default router
