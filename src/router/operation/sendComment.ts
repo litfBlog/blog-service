e7ab5ffe1e44ba296850f2bdac528da3b4ff437a
Litfa <admin@litf.com.cn>
2022-04-05 15:46:43 +0800

    feat(sendComments): 发送评论添加父亲字段

--- src/router/operation/sendComment.ts
+++ src/router/operation/sendComment.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: litfa
  * @Date: 2022-04-04 19:19:40
- * @LastEditTime: 2022-04-05 13:51:11
+ * @LastEditTime: 2022-04-05 14:49:32
  * @LastEditors: litfa
  * @Description: 发送评论
  * @FilePath: /blog-service/src/router/operation/sendComment.ts
@@ -12,15 +12,18 @@ import query from './../..//db/query'
 const router = Router()
 
 router.post('/add', async (req, res) => {
-  const { content, id, parent = 0 } = req.body
+  const { content, id, parent = 0, father = 0 } = req.body
   // parent 父级评论的id 用于回复评论 默认为0 直接发送评论
+  // father 最大评论的id
   const user = req.user as any
+  if (parent >= 1 && father == 0) return res.send({ status: 4 })
   const [err, results] = await query('INSERT INTO `comment` SET ?', {
     user_id: user.id,
     articles_id: id,
     content: content,
     status: 1,
-    parent
+    parent,
+    father
   })
   if (err) return res.send({ status: 5 })
   res.send({ status: 1, content_id: results.insertId })
