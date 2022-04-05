c8483f8226ec1ae44c9137fa3879ce4b6c7320c3
Litfa <admin@litf.com.cn>
2022-04-05 15:49:38 +0800

    feat(sendComments): 获取评论列表

---
+++ src/router/operation/getComment.ts
@@ -0,0 +1,75 @@
+/*
+ * @Author: litfa
+ * @Date: 2022-04-05 14:11:15
+ * @LastEditTime: 2022-04-05 15:45:52
+ * @LastEditors: litfa
+ * @Description: 获取评论列表
+ * @FilePath: /blog-service/src/router/operation/getComment.ts
+ * 
+ */
+import { Router } from 'express'
+import query from './../..//db/query'
+const router = Router()
+
+/**
+ * @description: 处理评论数据
+ * @param {*} commentList
+ * @return {*}
+ */
+class FormatCommentList {
+  // 原数据
+  private commentList: any = {}
+  // 处理后结果
+  private results: any = []
+  /**
+   * @description: 处理数据
+   * @param {*}
+   * @return {*} results
+   */
+  public format() {
+    this.commentList.forEach((element: any) => {
+      console.log(element)
+      // 父级评论 直接push进去
+      if (element.father == 0)
+        this.results.push({
+          ...element,
+          children: []
+        })
+      else
+        this.insertChied(element.father, element)
+    })
+    return this.results
+  }
+  // 处理子元素（由format函数调用）
+  private insertChied(father: number, element: any) {
+    for (const i in this.results) {
+      const item = this.results[i]
+      if (item.id == father) {
+        item.children.push({ ...element })
+        break
+      }
+    }
+  }
+  constructor(commentList: any) {
+    this.commentList = commentList
+  }
+}
+
+/**
+ * @description: 获取视频下的评论列表
+ * @param {*} 
+ * @param {*} 
+ * @param {*} res
+ * @return {*}
+ */
+router.post('/getList', async (req, res) => {
+  const { id } = req.body
+  if (!id) return res.send({ status: 4 })
+  // 获取文章评论
+  const [err, results] = await query('SELECT * FROM `comment` WHERE articles_id=?', id)
+  if (err) return res.send({ status: 5 })
+  const commentList = new FormatCommentList(results).format()
+  res.send({ status: 1, data: commentList })
+})
+
+export default router
