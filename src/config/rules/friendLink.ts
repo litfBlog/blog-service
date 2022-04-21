b43b71a3d3b4771f20feb611f86090ca4338effe
Litfa <admin@litf.com.cn>
2022-04-21 18:02:35 +0800

    fix: 修改规则

--- src/config/rules/friendLink.ts
+++ src/config/rules/friendLink.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: litfa
  * @Date: 2022-04-20 19:32:40
- * @LastEditTime: 2022-04-21 17:20:29
+ * @LastEditTime: 2022-04-21 17:51:46
  * @LastEditors: litfa
  * @Description: 友链表单规则
  * @FilePath: /blog-service/src/config/rules/friendLink.ts
@@ -13,7 +13,7 @@ const schema = Joi.object({
   name: Joi.string().required().min(2).max(10),
   url: Joi.string().required().min(4).max(50),
   desc: Joi.string().max(20),
-  icon: Joi.string().max(100)
+  icon: Joi.string().max(200)
 })
 
 export default schema
