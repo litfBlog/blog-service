60af3a9f04c87e2d98d4281d36bc22a68e658cb8
Litfa <admin@litf.com.cn>
2022-04-23 16:48:52 +0800

    fix: 允许没有封面

--- src/config/rules/articles.ts
+++ src/config/rules/articles.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: litfa
  * @Date: 2022-04-19 15:31:35
- * @LastEditTime: 2022-04-22 19:53:22
+ * @LastEditTime: 2022-04-23 16:46:29
  * @LastEditors: litfa
  * @Description: 文章验证规则
  * @FilePath: /blog-service/src/config/rules/articles.ts
@@ -13,7 +13,7 @@ const schema = Joi.object({
   id: Joi.number().required(),
   title: Joi.string().min(2).max(40).required(),
   content: Joi.string().min(10).max(100000).required(),
-  cover: Joi.string().max(200).required(),
+  cover: Joi.string().max(200).empty(''),
   desc: Joi.string().min(2).max(60).required()
 })
 
