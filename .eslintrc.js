a7b8b528486145cef970f73bfcf4ce7afa02a5c1
Litfa <admin@litf.com.cn>
2022-04-23 18:12:05 +0800

    chroe: eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,7 @@ module.exports = {
 
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off'
   }
 }
