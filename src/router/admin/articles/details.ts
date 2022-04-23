fd1f646ce6e1d0b24a967336985629b32be3ce47
Litfa <admin@litf.com.cn>
2022-04-23 16:29:17 +0800

    fix: 修改sql查询语句

--- src/router/admin/articles/details.ts
+++ src/router/admin/articles/details.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: litfa
  * @Date: 2022-04-10 19:28:00
- * @LastEditTime: 2022-04-10 19:50:43
+ * @LastEditTime: 2022-04-23 16:19:34
  * @LastEditors: litfa
  * @Description: 获取文章详情
  * @FilePath: /blog-service/src/router/admin/articles/details.ts
@@ -25,14 +25,14 @@ FROM
 LEFT JOIN \`likes\` likes ON articles.id = likes.articles_id AND likes.like=1
 LEFT JOIN \`users\` author ON articles.\`author\` = author.\`id\`
 LEFT JOIN likes is_liked ON is_liked.\`articles_id\` = articles.\`id\` AND is_liked.\`user_id\` = ?
-WHERE articles.id=? AND articles.status=0
+WHERE articles.id=?
 GROUP BY articles.id
-ORDER BY articles.createDate DESC
+ORDER BY articles.create_date DESC
 `
 router.post('/', async (req, res) => {
   const { id } = req.body
   const [err, results] = await query(sql, [0, id])
-  if (err) res.send({ status: 5 })
+  if (err) return res.send({ status: 5 })
   res.send({ status: 1, data: results })
 })
 
