8f19e11392cf303d30e907c0989c7a3447c42462
Litfa <admin@litf.com.cn>
2022-04-26 19:46:55 +0800

    feat(report): 举报

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: litfa
  * @Date: 2022-03-01 10:24:38
- * @LastEditTime: 2022-04-23 12:34:21
+ * @LastEditTime: 2022-04-26 19:17:04
  * @LastEditors: litfa
  * @Description: 路由
  * @FilePath: /blog-service/src/router/index.ts
@@ -42,6 +42,9 @@ router.use('/articles/sendComment', sendComment)
 router.use('/articles/getComment', getComment)
 router.use('/articles/getLiks', getLiks)
 router.use('/articles/likeComment', likeComment)
+
+import report from './operation/report'
+router.use('/report', report)
 
 // 友链
 import friendLink from './operation/friendLink'
