3f89682f8924e0f6a42f0537bc9d62ec6e95251f
Litfa <admin@litf.com.cn>
2022-04-28 18:02:14 +0800

    feat(admin/users/getUsers): 获取用户详细信息

--- src/router/admin/users/getUsers.ts
+++ src/router/admin/users/getUsers.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: litfa
  * @Date: 2022-04-11 15:19:58
- * @LastEditTime: 2022-04-11 16:44:26
+ * @LastEditTime: 2022-04-28 17:41:24
  * @LastEditors: litfa
  * @Description: 获取用户列表
  * @FilePath: /blog-service/src/router/admin/users/getUsers.ts
@@ -15,6 +15,13 @@ router.post('/all', async (req, res) => {
   const [err, results] = await query('SELECT * FROM users')
   if (err) return res.send({ status: 5 })
   res.send({ status: 1, list: results })
+})
+
+router.post('/getOne', async (req, res) => {
+  const { id } = req.body
+  const [err, results] = await query('SELECT * FROM users WHERE ?', { id })
+  if (err) return res.send({ status: 5 })
+  res.send({ status: 1, data: results[0] })
 })
 
 export default router
