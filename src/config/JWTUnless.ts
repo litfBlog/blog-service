17196ab4b9f20cc6c01b72e9f99193d316589f71
Litfa <admin@litf.com.cn>
2022-04-28 17:25:41 +0800

    feat: 获取真实ip

--- src/config/JWTUnless.ts
+++ src/config/JWTUnless.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: litfa
  * @Date: 2022-03-15 10:31:02
- * @LastEditTime: 2022-04-23 18:58:27
+ * @LastEditTime: 2022-04-28 16:35:36
  * @LastEditors: litfa
  * @Description: tokek 无需认证
  * @FilePath: /blog-service/src/config/JWTUnless.ts
@@ -14,5 +14,9 @@ export default [
   /^\/static\//,
   new RegExp(`${config.baseUrl}/articles/getOne/[0-9]`),
   new RegExp(`^${config.baseUrl}/articles/getLiks`),
-  new RegExp(`^${config.baseUrl}/articles/getOne/getWXML/`)
+  new RegExp(`^${config.baseUrl}/articles/getOne/getWXML/`),
+  new RegExp(`^${config.baseUrl}/friendLink/getHomeLink`),
+  new RegExp(`^${config.baseUrl}/articles/getComment/getCounts`),
+  new RegExp(`^${config.baseUrl}/articles/getComment/getCounts`),
+  new RegExp(`^${config.baseUrl}/articles/getComment/getList`)
 ]
