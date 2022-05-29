27fd3ef5d945b40e1b3e2ccdbf5c951c8977b427
Litfa <admin@litf.com.cn>
2022-05-29 17:37:00 +0800

    feat: 获取头像挂件信息

--- src/router/user/getUserInfo.ts
+++ src/router/user/getUserInfo.ts
@@ -11,10 +11,25 @@ import { Router } from 'express'
 import query from '../../db/query'
 const router = Router()
 
+const sql = `
+select
+    users.id,
+    users.username,
+    users.avatar,
+    users.registerDate,
+    users.permissions,
+    users.status,
+    users.avatar_pendant as avatar_pendant_id,
+    avatar_pendant.name as avatar_pendant_name,
+    avatar_pendant.url as avatar_pendant_url
+from users
+left join avatar_pendant on avatar_pendant.id = users.id
+where users.id = ?
+`
 router.post('/', async (req, res) => {
   const user = req.user as any
   // 为了确保用户信息实时性 需要从数据库查询
-  const [err, request] = await query('select id, username, avatar, registerDate, permissions, status, avatar_pendant from users where id=?', [user.id])
+  const [err, request] = await query(sql, [user.id])
   if (err) return res.send({ status: 5 })
   res.send({ status: 1, userInfo: request[0] })
 })
