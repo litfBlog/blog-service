e942a9069519aabf1b0bc57ff4281f1f9c3322f2
LiTengFei <admin@litf.com.cn>
2022-08-15 17:57:08 +0800

    fix: 修复token不失效的问题

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -14,6 +14,8 @@ export default (data: object) => {
   // 在服务器端直接拼接上 Bearer 的前缀
   return 'Bearer ' + jwt.sign(data, config.jwtSecretKey, {
     // expiresIn: '10h' // token 有效期为 10 个小时
-    expiresIn: 1000 * 60 * 60 * 10 // token 有效期为 10 个小时
+    // 这里单位是秒 不是毫秒！
+    // 直接用 10d 表示10天
+    expiresIn: '10d' 
   })
 }
